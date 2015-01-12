2041e9d59916b1ff6271cc8ffad65629c1811cd1
Vincent Lyons <vincentlyons@Vincents-MacBook-Air.local>
2015-01-12 21:43:35 +0000

    scaffolded contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :contacts
+
   root to: 'visitors#index'  
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations"}
   resources :users
