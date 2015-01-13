f58ec760e7321c051c604081e0331afe471a729a
Vincent Lyons <vincentlyons@Vincents-MacBook-Air.local>
2015-01-13 02:32:23 +0000

    mainly css changes to the dashboard and first refactor of the create contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :educations
-
   resources :contacts
-
-  root to: 'visitors#index'  
+  root to: 'contacts#index'  
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations"}
   resources :users
 end
